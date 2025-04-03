db620789b2ea0a3fed4f004e40eeb38ed14aaeaf
Inflvx <seochoa@unicesar.edu.co>
2025-04-03 17:54:27 -0500

    Se creo el portafolio de inflvx dev

---
+++ src/data/projects.js
@@ -0,0 +1,61 @@
+
+export const projects = [
+    {
+      title: "Selenium ChatBot",
+      description: "Script que permite la automatizacion de mensajes en WhatsApp para la consulta y recordatorio de citas medicas",
+      tags: ["Python", "Supabase", "Selenium"],
+      image: "./src/assets/projects/asmuycavaapp.png",
+      repo: "https://github.com/InflvxDev/Selenium-Wpp-Messages-Automatic"
+    },
+    {
+      title: "ASMUYCAVA App",
+      description: "Aplicación móvil para gestionar la trazabilidad del proceso del café para la asociación de mujeres cafeteras valledupar ASMUYCAVA.",
+      tags: ["Flutter", "Supabase", "Dart"],
+      image: "./src/assets/projects/asmuycavaapp.png",
+    },
+    {
+      title: "Gneis AppWeb",
+      description: "Aplicación web para la gestión de habitaciones y reservas del hotel Neruda.",
+      tags: ["Angular", ".Net", "SupaBase"],
+      image: "./src/assets/projects/gneisweb.png",
+      repo: "https://github.com/InflvxDev/GneisTest"
+    },
+    {
+        title: "WikiCinema",
+        description: "Aplicativo Movil, Que sirve como wikipedia de peliculas, mediante el uso del Api MovieDB.",
+        tags: ["Flutter", "Dart"],
+        image: "/images/projects/ecommerce.jpg",
+        repo: "https://github.com/InflvxDev/WikiCinema"
+    },
+    {
+      title: "Eotia Cake",
+      description: "Aplicativo Web, para la gestion de productos de la pasteleria Eotia.",
+      tags: ["Java", "SpringBoot"],
+      image: "/images/projects/ecommerce.jpg",
+      repo: "https://github.com/Jmanriques/Proyecto_EOTIA_CAKE/tree/main/EOTIA_CAKE_APP"
+    },
+    {
+      title: "JimmyMultiPercetron",
+      description: "Aplicativo Web, para la prueba de un modelo de inteligencia artificial multineuronal para la optimizacion de rutas.",
+      tags: ["Angular"],
+      repo: "https://github.com/InflvxDev/JimmyAG"
+    },
+    {
+      title: "SyZops",
+      description: "Datapack para minecraft 1.19 que añade variantes de diferentes tipos de mobs como arañas, zombies, creepers y esqueletos.",
+      tags: ["McFunction"],
+      repo: "https://github.com/InflvxDev/SyZops"
+    },
+    {
+      title: "You lose hearts if Death",
+      description: "Datapack para minecraft 1.19 que añade la funcionalidad de perder un corazon cada vez que mueras.",
+      tags: ["McFunction"],
+      repo: "https://github.com/InflvxDev/you-lose-hearts-if-death"
+    },
+    {
+      title: "NeutrAgressive",
+      description: "Datapack para minecraft 1.19 que añade la funcionalidad de volver los mobs neutrales agresivos.",
+      tags: ["McFunction"],
+      repo: "https://github.com/InflvxDev/NeutrAgressive"
+    }
+  ];
