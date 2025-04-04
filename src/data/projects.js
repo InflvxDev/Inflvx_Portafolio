6f4ad685c78db91111a43f4449d0f9ed2e721da2
Inflvx <seochoa@unicesar.edu.co>
2025-04-04 17:34:32 -0500

    correcion en los proyectos

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,8 @@ export const projects = [
     title: "Selenium ChatBot",
     description: "Script que automatiza el envío de mensajes en WhatsApp para la consulta y recordatorio de citas médicas.",
     tags: ["Python", "Supabase", "Selenium"],
-    repo: "https://github.com/InflvxDev/Selenium-Wpp-Messages-Automatic"
+    image: "./src/assets/projects/asmuycavaapp.png",
+    repo: "https://github.com/InflvxDev/Selenium-Wpp-Messages-Automatic",
   },
   {
     title: "ASMUYCAVA App",
@@ -36,24 +37,28 @@ export const projects = [
     title: "JimmyMultiPerceptron",
     description: "Aplicación web para probar un modelo de inteligencia artificial multineuronal enfocado en la optimización de rutas.",
     tags: ["Angular"],
+    image: "./src/assets/projects/asmuycavaapp.png",
     repo: "https://github.com/InflvxDev/JimmyAG"
   },
   {
     title: "SyZops",
     description: "Datapack para Minecraft 1.19 que agrega variantes de diferentes mobs, como arañas, zombis, creepers y esqueletos.",
     tags: ["McFunction"],
+    image: "./src/assets/projects/asmuycavaapp.png",
     repo: "https://github.com/InflvxDev/SyZops"
   },
   {
     title: "You Lose Hearts if Death",
     description: "Datapack para Minecraft 1.19 que reduce un corazón de vida cada vez que el jugador muere.",
     tags: ["McFunction"],
+    image: "./src/assets/projects/asmuycavaapp.png",
     repo: "https://github.com/InflvxDev/you-lose-hearts-if-death"
   },
   {
     title: "NeutrAgressive",
     description: "Datapack para Minecraft 1.19 que hace que los mobs neutrales se vuelvan agresivos.",
     tags: ["McFunction"],
+    image: "./src/assets/projects/asmuycavaapp.png",
     repo: "https://github.com/InflvxDev/NeutrAgressive"
   }
 ];
