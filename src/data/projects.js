d0e4e198365fa00c8a2068fb1cfef7341e875d0a
Inflvx <seochoa@unicesar.edu.co>
2025-04-24 14:37:18 -0500

    Update project images and enhance ProjectCard component with carousel functionality

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,64 +1,57 @@
 export const projects = [
   {
+    title: "OHI Hokoku",
+    description: "Script que automatiza el envío de mensajes en WhatsApp para la consulta y recordatorio de citas médicas.",
+    tags: ["Astro", "React", "Supabase", "SqlServer"],
+    images: ["/projects/hokokulogin.png", "/projects/hokokudash.png", "/projects/hokokureport.png"],
+    repo: "https://github.com/InflvxDev/Selenium-Wpp-Messages-Automatic",
+  },
+  {
     title: "Selenium ChatBot",
     description: "Script que automatiza el envío de mensajes en WhatsApp para la consulta y recordatorio de citas médicas.",
     tags: ["Python", "Supabase", "Selenium"],
-    image: "./src/assets/projects/asmuycavaapp.png",
+    images: ["/projects/seleniumbot.png", "/projects/seleniumbot2.png"],
     repo: "https://github.com/InflvxDev/Selenium-Wpp-Messages-Automatic",
   },
   {
     title: "ASMUYCAVA App",
     description: "Aplicación móvil para gestionar la trazabilidad del proceso del café en la Asociación de Mujeres Cafeteras de Valledupar (ASMUYCAVA).",
     tags: ["Flutter", "Supabase", "Dart"],
-    image: "./src/assets/projects/asmuycavaapp.png"
+    images: ["/projects/asmuycavalist.png","/projects/asmuycavaapp.png", "/projects/asmuycavadatos.png", "/projects/asmuycavaperfil.png"],
   },
   {
     title: "Gneis AppWeb",
     description: "Aplicación web para la gestión de habitaciones y reservas en el Hotel Neruda.",
     tags: ["Angular", ".NET", "Supabase"],
-    image: "./src/assets/projects/gneisweb.png",
+    images: ["/projects/gneis.png", "/projects/gneisadmin.png","/projects/gneisuserreg.png","/projects/gneishab.png","/projects/gneisclient.png"],
     repo: "https://github.com/InflvxDev/GneisTest"
-  },
-  {
-    title: "WikiCinema",
-    description: "Aplicación móvil que funciona como una Wikipedia de películas, utilizando la API de The Movie Database (TMDb).",
-    tags: ["Flutter", "Dart"],
-    image: "/images/projects/ecommerce.jpg",
-    repo: "https://github.com/InflvxDev/WikiCinema"
   },
   {
     title: "Eotia Cake",
     description: "Aplicación web para la gestión de productos de la pastelería Eotia.",
     tags: ["Java", "Spring Boot"],
-    image: "/images/projects/ecommerce.jpg",
+    images: ["/projects/eotiacake.png","/projects/eotiacakeespeciales.png", "/projects/eotiacakeproductos.png"],
     repo: "https://github.com/Jmanriques/Proyecto_EOTIA_CAKE/tree/main/EOTIA_CAKE_APP"
-  },
-  {
-    title: "JimmyMultiPerceptron",
-    description: "Aplicación web para probar un modelo de inteligencia artificial multineuronal enfocado en la optimización de rutas.",
-    tags: ["Angular"],
-    image: "./src/assets/projects/asmuycavaapp.png",
-    repo: "https://github.com/InflvxDev/JimmyAG"
   },
   {
     title: "SyZops",
     description: "Datapack para Minecraft 1.19 que agrega variantes de diferentes mobs, como arañas, zombis, creepers y esqueletos.",
     tags: ["McFunction"],
-    image: "./src/assets/projects/asmuycavaapp.png",
+    images: ["/projects/datapacklogo.png"],
     repo: "https://github.com/InflvxDev/SyZops"
   },
   {
     title: "You Lose Hearts if Death",
     description: "Datapack para Minecraft 1.19 que reduce un corazón de vida cada vez que el jugador muere.",
     tags: ["McFunction"],
-    image: "./src/assets/projects/asmuycavaapp.png",
+    images: ["/projects/datapacklogo.png"],
     repo: "https://github.com/InflvxDev/you-lose-hearts-if-death"
   },
   {
     title: "NeutrAgressive",
     description: "Datapack para Minecraft 1.19 que hace que los mobs neutrales se vuelvan agresivos.",
     tags: ["McFunction"],
-    image: "./src/assets/projects/asmuycavaapp.png",
+    images: ["/projects/datapacklogo.png"],
     repo: "https://github.com/InflvxDev/NeutrAgressive"
   }
 ];
