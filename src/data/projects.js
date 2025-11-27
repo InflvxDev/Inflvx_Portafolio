71cf250c834ea203b4699680da53e2221ebea2d3
InflvxDev <seochoa@unicesar.edu.co>
2025-11-27 10:02:55 -0500

    feat: replace PNG images with WEBP format for improved performance; update project data in projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,41 +1,49 @@
 export const projects = [
   {
-    title: "OHI BloodShoot BS y STF",
-    description: "Plataforma integral para el registro y seguimiento de folios de banco de sangre y componentes de servicio transfusional de la organizacion humana integral OHI.",
-    tags: ["Astro", "React", "Supabase", "SqlServer"],
-    images: ["/projects/bloodshotlogin.webp", "/projects/bloodshotfolioreg.webp", "/projects/bloodshotingcomp.webp", "/projects/bloodshotegrcomp.webp"],
+    title: "Kenwa",
+    description: "Aplicativo movil para el control del tiempo de trabajo y descanso laboral. Asi como el seguimiento del nivel de estres del empleado.",
+    tags: ["Flutter", "Dart"],
+    images: ["/projects/kenwa.webp"],
+    web: "https://kenwa.pages.dev/",
+  
   },
   {
-    title: "OHI Hokoku",
-    description: "Plataforma integral para la gestión y seguimiento de información de las áreas de servicio de la organizacion humana integral OHI.",
+    title: "Hokoku",
+    description: "Plataforma integral para la gestión y seguimiento de información de las áreas administrativas de la organizacion humana integral OHI.",
     tags: ["Astro", "React", "Supabase", "SqlServer"],
-    images: ["/projects/hokokulogin.jpg", "/projects/hokokudash.webp", "/projects/hokokureport.webp"],
+    images: ["/projects/hokokulogin.webp", "/projects/hokokuwelcomeadmin.webp", "/projects/hokokuwelcomeusers.webp"],
+  },
+  {
+    title: "Hokoku BloodShot",
+    description: "Extension de hokoku para el registro y seguimiento de folios de banco de sangre, ademas control de inventiario para los componentes de servicio transfusional.",
+    tags: ["Astro", "React", "Supabase", "SqlServer"],
+    images: ["/projects/bloodshotlogin.webp", "/projects/bloodshotfolioreg.webp", "/projects/bloodshotingcomp.webp", "/projects/bloodshotegrcomp.webp", "/projects/bloodshotcomponentesinv.webp"],
   },
   {
     title: "Selenium ChatBot",
     description: "Script que automatiza el envío de mensajes en WhatsApp para la consulta y recordatorio de citas médicas.",
-    tags: ["Python", "Supabase", "Selenium"],
-    images: ["/projects/seleniumbot.png", "/projects/seleniumbot2.png"],
+    tags: ["Python", "Selenium"],
+    images: ["/projects/seleniumbot.webp", "/projects/seleniumbot2.webp"],
     repo: "https://github.com/InflvxDev/Selenium-Wpp-Messages-Automatic",
   },
   {
     title: "ASMUYCAVA App",
     description: "Aplicación móvil para gestionar la trazabilidad del proceso del café en la Asociación de Mujeres Cafeteras de Valledupar (ASMUYCAVA).",
     tags: ["Flutter", "Supabase", "Dart"],
-    images: ["/projects/asmuycavalist.png","/projects/asmuycavaapp.png", "/projects/asmuycavadatos.png", "/projects/asmuycavaperfil.png"],
+    images: ["/projects/asmuycavalist.webp","/projects/asmuycavaapp.webp", "/projects/asmuycavadatos.webp", "/projects/asmuycavaperfil.webp"],
   },
   {
     title: "Gneis AppWeb",
     description: "Aplicación web para la gestión de habitaciones y reservas en el Hotel Neruda.",
-    tags: ["Angular", ".NET", "Supabase"],
-    images: ["/projects/gneis.png", "/projects/gneisadmin.png","/projects/gneisuserreg.png","/projects/gneishab.png","/projects/gneisclient.png"],
+    tags: ["Angular", ".NET", "SQL Server"],
+    images: ["/projects/gneis.webp", "/projects/gneisadmin.webp","/projects/gneisuserreg.webp","/projects/gneishab.webp","/projects/gneisclient.webp"],
     repo: "https://github.com/InflvxDev/GneisTest"
   },
   {
     title: "Eotia Cake",
     description: "Aplicación web para la gestión de productos de la pastelería Eotia.",
     tags: ["Java", "Spring Boot"],
-    images: ["/projects/eotiacake.png","/projects/eotiacakeespeciales.png", "/projects/eotiacakeproductos.png"],
+    images: ["/projects/eotiacake.webp","/projects/eotiacakeespeciales.webp", "/projects/eotiacakeproductos.webp"],
     repo: "https://github.com/Jmanriques/Proyecto_EOTIA_CAKE/tree/main/EOTIA_CAKE_APP"
   },
 ];
