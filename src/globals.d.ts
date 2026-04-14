685bb59bef6ed1838f29059b6d57acb625ea985c
InflvxDev <seochoa@unicesar.edu.co>
2026-04-14 09:46:28 -0500

    feat: add Inter Variable font support and preload font for improved performance

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,3 +1,11 @@
 declare module "*.css";
 declare module "@fontsource/*" {}
 declare module "@fontsource-variable/*" {}
+declare module "@fontsource-variable/*?url" {
+  const url: string;
+  export default url;
+}
+declare module "*.woff2?url" {
+  const url: string;
+  export default url;
+}
